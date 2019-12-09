40e2710ff8f400fbd0ff52f5f66a88ec9a2401f9
DefinitelyNotAShark <definitelynotasharkohno@gmail.com>
2019-12-09 09:09:39 -0600

    Added terrain and fireflies for immersion

---
+++ ECSSamplesClone/Assets/MoveCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+         Vector3 vector = new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
+
+        transform.Rotate(vector);
+    }
+}
